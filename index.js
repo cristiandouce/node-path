4bb5a2fa9cb266e3f1014191f2e181b5be629285
Cristian Douce <cristian@gravityonmars.com>
2013-10-18 20:40:21 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Extends module's paths with
+ * process' mainModule's paths.
+ *
+ * @param {Object} m module to extend
+ * @api public
+ */
+
+module.exports = function(m, paths) {
+  if (paths.length) return m.paths.concat(paths);
+  process.mainModule.paths.forEach(function(p) {
+    if (~m.paths.indexOf(p)) return;
+    m.paths.push(p);
+  });
+}
