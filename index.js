26c188cc1d115187bed328d38bf9192945146630
Cristian Douce <cristian@gravityonmars.com>
2013-10-19 00:10:36 -0300

    Check pahts exists before paths.length

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  */
 
 module.exports = function(m, paths) {
-  if (paths.length) return m.paths.unshift.apply(m.paths, paths);
+  if (paths && paths.length) return m.paths.unshift.apply(m.paths, paths);
   process.mainModule.paths.forEach(function(p) {
     if (~m.paths.indexOf(p)) return;
     m.paths.push(p);
